1eb2e64c984227debbbd704c1b40075eb51dfbe8
okosiuta <okosiuta@corevalue.net>
2019-07-26 10:13:04 +0300

    remove redundant properties from enums

--- src/main/java/net/corevalue/app/constant/DeviceType.java
+++ src/main/java/net/corevalue/app/constant/DeviceType.java
@@ -1,12 +1,5 @@
 package net.corevalue.app.constant;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public enum DeviceType {
-    RASPBERRY("Raspberry Pi");
-
-    private String value;
+    RASPBERRY
 }
--- src/main/java/net/corevalue/app/constant/SensorType.java
+++ src/main/java/net/corevalue/app/constant/SensorType.java
@@ -1,13 +1,5 @@
 package net.corevalue.app.constant;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public enum SensorType {
-    CO2_SENSOR("Co2 Sensor"), HUMIDITY_SENSOR("Humidity Sensor"),
-    TEMPERATURE_SENSOR("Temperature Sensor"), LIGHT_SENSOR("Light Sensor");
-
-    private String value;
+    CO2_SENSOR, HUMIDITY_SENSOR, TEMPERATURE_SENSOR, LIGHT_SENSOR
 }
